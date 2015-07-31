e3a03b90646740fdf393cd1274ab1144cc1daf8c
James Young <pronoiac@gmail.com>
2015-07-31 16:52:08 -0700

    Parsing the family tree data

--- family.rb
+++ family.rb
@@ -9,18 +9,27 @@ class Family
 end # /family
 
 class Person
-    def initalize(name, parent=nil)
+    def initialize(name, parent=nil)
         @name = name
         @parent = parent
     end
+    
+    def name
+        @name
+    end
 end # /person
+
+the_fam = Family.new
 
 # let's parse the family tree
 tree_file = open("family.txt", "r")
 tree_file.each_line { |line|
     line = line.chomp
     next if line =~ /^#/
-    p line
+    child, parent = line.split(", ")
+    puts "#{parent} begat #{child}"
+    peep = Person.new(child, parent)
+    the_fam.add(peep)
 }
 
-
+p the_fam
