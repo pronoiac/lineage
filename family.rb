fb0211889bc7d7c683004d73ae06de659662602e
James Young <pronoiac@gmail.com>
2015-07-31 17:11:26 -0700

    Rearranging class hierarchy
    It's tracking parents and children properly now.

--- family.rb
+++ family.rb
@@ -3,22 +3,41 @@ class Family
         @family = {}
     end
     
-    def add(person)
-        @family[person.name] = person
+    def add_parentage(child_name, parent_name)
+        child = find_or_create(child_name)
+        parent = find_or_create(parent_name)
+        child.parent = parent_name
+        parent.add_child(child_name)
+    end
+    
+    def find_or_create(name)
+        return @family[name] if @family.key?(name)
+        @family[name] = Person.new(name)
+        @family[name]
     end
 end # /family
 
 class Person
-    def initialize(name, parent=nil)
+    def initialize(name)
         @name = name
-        @parent = parent
+        @parent = nil
+        @children = []
     end
     
     def name
         @name
     end
+    
+    def parent=(rent)
+        @parent = rent
+    end
+    
+    def add_child(child_name)
+        @children << child_name
+    end
 end # /person
 
+# main
 the_fam = Family.new
 
 # let's parse the family tree
@@ -28,8 +47,7 @@ tree_file.each_line { |line|
     next if line =~ /^#/
     child, parent = line.split(", ")
     puts "#{parent} begat #{child}"
-    peep = Person.new(child, parent)
-    the_fam.add(peep)
+    the_fam.add_parentage(child, parent)
 }
 
 p the_fam
