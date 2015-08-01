0196f3c153d5412821688ee3b3ee8ea98c78e833
James Young <pronoiac@gmail.com>
2015-07-31 19:34:58 -0700

    Using attr_reader and _accessor for getters and setters
    I verified that it works in Ruby 2.2 and 1.9.3.

--- person.rb
+++ person.rb
@@ -1,17 +1,13 @@
 class Person
+    attr_accessor :parent
+    attr_reader :name, :children
+    
     def initialize(name)
         @name = name
         @parent = nil
         @children = []
     end
     
-    def name
-        @name
-    end
-    
-    def parent=(rent)
-        @parent = rent
-    end
     
     def add_child(child_name)
         @children << child_name
