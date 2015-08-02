173a58f349aa2b356b23370dfdd1e5ef29d2713d
James Young <pronoiac@gmail.com>
2015-08-02 14:40:50 -0700

    Adding #show_grandparents

--- lineage.rb
+++ lineage.rb
@@ -29,11 +29,43 @@ EOM
     gets.chomp
 end # /menu
 
+
+def list_members()
+    results = []
+    @the_fam.list_members.each { |peep_name|
+        peep = @the_fam.lookup(peep_name)
+        results << peep.name
+    }
+    puts results.sort.join(", ")
+end
+
+
+def show_grandparent()
+    puts "Showing a grandparent."
+    choice = false
+    
+    while !choice
+        print "Whose grandparent do you want to know? "
+        name = gets.chomp
+        choice = @the_fam.lookup(name)
+        if !choice
+            puts "Unknown member. Known family members: "
+            list_members
+        end
+    end # /choice
+
+    parent = @the_fam.lookup(choice.parent)
+    
+    puts "#{parent.parent} is the grandparent of #{name}.\n"
+end
+
+
 prompting = true
 while prompting
     choice = menu
     case choice
     when "1" # get grandparent for someone
+        show_grandparent
     when "2" # list of no siblings
     when "3" # list of no children
     when "4" # who has most grandchildren?
