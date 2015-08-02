7c0aa9080a4265bcb6513ae0c72d985908b20936
James Young <pronoiac@gmail.com>
2015-08-02 14:09:47 -0700

    Sketching out menu

--- family_tree.rb
+++ family_tree.rb
@@ -1,5 +1,6 @@
 require_relative "family"
 require_relative "person"
+# require "byebug"
 
 # main
 the_fam = Family.new
@@ -10,8 +11,37 @@ tree_file.each_line { |line|
     line = line.chomp
     next if line =~ /^#/
     child, parent = line.split(", ")
-    puts "#{parent} begat #{child}"
+    # puts "#{parent} begat #{child}"
     the_fam.add_parentage(child, parent)
 }
 
-p the_fam
+def menu
+    puts <<EOM
+What do you want me to do?
+    1. Given a name, I'll tell you their grandparent.
+    2. List people without siblings.
+    3. List people without children.
+    4. Tell you who has the most grandchildren.
+    5. Draw the family tree. (not available)
+    6 / x / q. exit
+EOM
+    print "Choice? "
+    gets.chomp
+end # /menu
+
+prompting = true
+while prompting
+    choice = menu
+    case choice
+    when "1" # get grandparent for someone
+    when "2" # list of no siblings
+    when "3" # list of no children
+    when "4" # who has most grandchildren?
+    when "5" # draw family tree
+    when "6", "q", "x" # exit
+        puts "Exiting."
+        prompting = false
+    else # not recognized
+        puts "Choice not recognized. Options: [123456q]"
+    end
+end # /prompting for choice
